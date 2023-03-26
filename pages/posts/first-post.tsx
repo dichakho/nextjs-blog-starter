ab500cf31102c3f767222c6c9fe5406bbe7716cf
dichakho <dichakho@gmail.com>
2023-03-26 23:14:09 +0700

    feat: config absolute path

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,8 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import Script from 'next/script';
-import Layout from '../../components/layout';
+import Layout from '@/components/layout';
 
 export default function FirstPost() {
   return (
